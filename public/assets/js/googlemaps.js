6892583402071d73fcbbe1198ff963cb499d49f1
Cants820 <Cantosjoseph@gmail.com>
2017-11-08 08:53:12 -0800

    finished

--- public/assets/js/googlemaps.js
+++ public/assets/js/googlemaps.js
@@ -1,51 +1,39 @@
+var activity = [
+   ['Garbage Clean Up'], ['Beach Clean Up']
+]
 
-var loc = [];
-// console.log(loc)
-for (var i = 0; i < loc.length; i++) {
-}
+var markers = [
+    ['San Francisco', 37.7749, -122.431297],
+    ['Ocean Beach', 37.7594, -122.5107]
+];
+
+var date = [
+    ['2017-12-12 07:00:00', '2017-12-13 06:30:00']
+]
+
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: { lat: 37.7749, lng: -122.431297 }
-  });
-  var geocoder = new google.maps.Geocoder();
-  var infowindow = new google.maps.InfoWindow;
-  document.getElementById('submit').addEventListener('click', function (event) {
-    event.preventDefault()
-    geocodeAddress(geocoder, map, infowindow);
-    // console.log(geocoder)
-    // console.log(infowindow)
-    // console.log(map)
-  });
-  var newAutocomplete = new google.maps.places.Autocomplete(document.getElementById('address'), { types: ['geocode'] });
-  // autocomplete.setComponentRestrictions({ 'country': ['us'] });
-  newAutocomplete.addListener('place_changed', () => {
-    // self.zone.run(() => {
-    // this._address = autocomplete.getPlace();
-    // console.log(newAutocomplete);
-    // });
-  });
-}
-function geocodeAddress(geocoder, resultsMap, infowindow) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({ 'address': address }, function (results, status) {
-    // var latlng = [(results[0].geometry.bounds.f.f),(results[0].geometry.bounds.b.b)]
-    // console.log(latlng);
-
-    if (status === 'OK') {
-      var marks = (results[0].formatted_address)
-      console.log(marks)
-      loc.push(marks);
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        animation: google.maps.Animation.DROP,
-        position: results[0].geometry.location
-      });
-      infowindow.setContent(results[0].formatted_address);
-      infowindow.open(map, marker);
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+    var latlng = new google.maps.LatLng(37.7749, -122.431297);
+    var myOptions = {
+        zoom: 12,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false
+    };
+    var map = new google.maps.Map(document.getElementById("map"),myOptions);
+    var infowindow = new google.maps.InfoWindow(), marker, i;
+    for (i = 0; i < markers.length; i++) {  
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+            map: map
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent('<div><strong>' + 'Event: ' + activity[i][0] + '</strong><br>' +
+                markers[i][0] + '<br>' +
+                'Date: ' + date[i] + '</div>');
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
     }
-  });
 }
